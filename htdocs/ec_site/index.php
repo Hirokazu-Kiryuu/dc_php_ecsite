f2289e5a6f39c830116d244b66a864e138b41096
kiryuu <kiryuhirokazu@gmail.com>
2023-05-15 13:43:00 +0900

    2回目のコミット

--- htdocs/ec_site/index.php
+++ htdocs/ec_site/index.php
@@ -1,17 +1,25 @@
-  <?
-  echo TEST; 
-  
-  ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>INDEX</title>
+  <title>ログインページ</title>
 </head>
 <body>
+
+  <h1>ログイン</h1>
+  <form action="post" action="#">
+    <p>ユーザー名
+      <input type="text" name="user_name" value="">
+    </p>
+    <p>パスワード
+      <input type="text" name="password" value="">
+    </p>
+    <input type="submit" name="login" value="登録">
+    <br>
+  </form>
+  <a href="user.php">新規登録ページへ</a>
 
 </body>
 </html>
